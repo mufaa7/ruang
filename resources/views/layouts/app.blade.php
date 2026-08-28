7fb9393c88a249be4b2a6677b2335fbe07b12b9b
Mufaa7 <mu71faa@gmail.com>
2026-08-28 18:38:03 +0700

    fix(theme): ensure html dark class is always active by default across all layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <script>
-            // Prevent FOUC
-            if (localStorage.theme === 'dark') {
-                document.documentElement.classList.add('dark')
-            } else {
+            // Default to Dark Mode
+            if (localStorage.theme === 'light') {
                 document.documentElement.classList.remove('dark')
+            } else {
+                document.documentElement.classList.add('dark')
             }
         </script>
 
